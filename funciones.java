b86ad071b6202b71ea9eb4f2bdbc76dc5c795f74
Oliver <omiguezalonso@gmail.com>
2024-12-13 09:55:15 +0100

    Funciones que usaría

---
+++ funciones.java
@@ -0,0 +1,43 @@
+/**
+ * En este código explicaré las distintas funciones que utilizaría para crear este programa
+ * @author Oliver Miguez Alonso
+ * @version 1.0
+ */
+public class funciones {
+
+    /**
+     * En esta función definiria las variables básica como los contadores
+     * @param args no usamos
+     */
+    public static void main(String[] args) {
+        //Definimos el array del tablero 8x8
+    }//end main
+
+    /**
+     * En esta función escribiría el código con el que comprobaría si la posición en el tablero está ocupada o no
+     * y mencionar por cual jugador esta ocupada en caso de estarlo
+     * @param contador1 contador de fichas del jugador 1
+     * @param contador2 contador de fichas del jugador 2
+     * @param tablero  array original del tablero
+     */
+    static void comprobarOcupadaONo(int contador1, int contador2, int[][]tablero){
+
+    }//end comprobarOcupadaONo
+
+    /**
+     * Esta función escribiría el código con el que se comprobaría si hay algún ganador en la partida
+     * @param contador1 contador de fichas del jugador 1
+     * @param contador2 contador de fichas del jugador 2
+     */
+    static void comprobarVictoria(int contador1,int contador2){
+
+    }//end comprobarVictoria
+
+    /**
+     * Esta función escribiría el código con el que podremos crear una tabla , en este caso 8x8, pero esto nos
+     * permitiría crear una tabla de cualquier tamaño, al gusto del consumidor
+     */
+    static void crearTablero(){
+
+    }//end crearTablero
+}//end class
